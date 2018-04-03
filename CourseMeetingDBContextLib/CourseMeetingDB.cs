004e077719fe155d5b0033ddf468a27438369143
AlexGruebel <alexander-gruembel@online.de>
2018-04-03 22:23:32 +0200

    add CourseMeetingDB context class

---
+++ CourseMeetingDBContextLib/CourseMeetingDB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using CourseMeetingEntitiesLib;
+
+namespace CourseMeetingDBContextLib
+{
+    public class CourseMeetingDb : DbContext
+    {
+
+        public DbSet<Course> Courses {get;set;}
+        public DbSet<Course> CourseMeetings { get; set; }
+        public DbSet<Course> CoursesMeetingParticipants { get; set; }
+        public DbSet<Course> CourseSecundaryTeachers { get; set; }
+        public DbSet<Course> Roles { get; set; }
+        public DbSet<Course> Users { get; set; }
+
+        public CourseMeetingDb(DbContextOptions options) : base(options)
+        {
+           
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+           modelBuilder.Entity<Course>(c =>
+           {
+               c.HasKey(m => m.CID);
+
+               c.HasMany(m => m.CourseMeetings)
+               .WithOne(m => m.Course);
+
+               c.HasMany(st => st.STeachers)
+               .WithOne(st => st.Course);
+           });
+
+            modelBuilder.Entity<CourseMeeting>(cm =>
+            {
+                cm.HasKey(k => k.MID);
+            });
+
+            modelBuilder.Entity<User>(u =>
+            {
+                u.HasKey(k => k.Id);
+
+                u.Property(p => p.Id)
+                .HasColumnName("UID");
+            });
+
+            modelBuilder.Entity<CourseMeetingParticipant>(c =>
+            {
+                c.HasOne(m => m.CourseMeeting)
+                .WithMany(m => m.CourseMeetingParticipants);
+
+                c.HasOne(m => m.Student)
+                .WithMany(s => s.)
+            });
+
+                
+        }
+
+    }
+}
