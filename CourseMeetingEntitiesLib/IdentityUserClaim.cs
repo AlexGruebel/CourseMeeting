e8983cf4a08ee48df22a10535912f69b21f8eca1
AlexGruebel <alexander-gruembel@online.de>
2018-04-08 20:06:02 +0200

    IdentityUserClaim

---
+++ CourseMeetingEntitiesLib/IdentityUserClaim.cs
@@ -0,0 +1,9 @@
+namespace CourseMeetingEntitiesLib
+{
+    public class IdentityUserClaim{
+        public int ID {get;set;}
+        public string ClaimType {get;set;}
+        public string ClaimValue{get;set;}
+        public string UserID{get;set;}
+    }
+}
