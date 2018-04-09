ab32a5b089c2b05a643cd17c2d4920993f3f6c8f
AlexGruebel <alexander-gruembel@online.de>
2018-04-09 17:55:46 +0200

    removed unecessary property

--- CourseMeetingEntitiesLib/sec/Role.cs
+++ CourseMeetingEntitiesLib/sec/Role.cs
@@ -7,7 +7,7 @@ namespace CourseMeetingEntitiesLib.sec
 {
     public class Role : IdentityRole
     {
-        public int RID { get; set; }
+        //public int RID { get; set; }
         public string RName { get; set; }
         public string RDescription { get; set; }
 
