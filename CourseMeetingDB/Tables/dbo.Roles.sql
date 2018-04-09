76589146540fff93a473b9b216f289e173d713c3
AlexGruebel <alexander-gruembel@online.de>
2018-04-09 07:34:36 +0200

    change the schema

--- CourseMeetingDB/Tables/dbo.Roles.sql
+++ CourseMeetingDB/Tables/dbo.Roles.sql
@@ -1,6 +1,12 @@
-﻿CREATE TABLE [dbo].[Roles]
+﻿CREATE TABLE [sec].[Roles](
+	[RID] [int] NOT NULL,
+	[RName] [nvarchar](20) NOT NULL,
+	[RDescription] [nvarchar](100) NOT NULL
+) ON [PRIMARY]
+
+GO
+ALTER TABLE [sec].[Roles] ADD PRIMARY KEY CLUSTERED 
 (
-	 [RID] int Primary Key
-	,[RName] nvarchar(20) NOT NULL
-	,[RDescription] nvarchar(100) NOT null
-)
+	[RID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, SORT_IN_TEMPDB = OFF, IGNORE_DUP_KEY = OFF, ONLINE = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+GO
