450bcea7536d4e5321f345387e9af1554f7a8275
AlexGruebel <alexander-gruembel@online.de>
2018-04-20 07:33:35 +0200

    removed old code

--- CourseMeetingEntitiesLib/sec/Role.cs
+++ CourseMeetingEntitiesLib/sec/Role.cs
@@ -7,11 +7,7 @@ namespace CourseMeetingEntitiesLib.sec
 {
     public class Role : IdentityRole
     {
-        //public int RID { get; set; }
         public string RName { get; set; }
         public string RDescription { get; set; }
-
-        //public User User {get;set;}
-        //public IdentityUserRole IdentityUserRole {get;set;}
     }
 }
