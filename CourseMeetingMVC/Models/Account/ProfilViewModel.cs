912bde303e97bf048209e4ed6432288111e997ff
AlexGruebel <alexander-gruembel@online.de>
2018-04-20 07:36:03 +0200

    add ProfilVIewModel

---
+++ CourseMeetingMVC/Models/Account/ProfilViewModel.cs
@@ -0,0 +1,10 @@
+namespace CourseMeetingMVC.Models.Account
+{
+    public class ProfilViewModel{
+        public string UserName{get;set;}
+        public string EMail {get;set;}
+        public string PhoneNumber{get;set;}
+
+        public string Role {get;set;}
+    }
+}
