6e2f3d85b48ee2096ba6e22033d66236512d554c
AlexGruebel <alexander-gruembel@online.de>
2018-04-20 07:32:18 +0200

    fixed property name

--- CourseMeetingEntitiesLib/dbo/CourseMeeting.cs
+++ CourseMeetingEntitiesLib/dbo/CourseMeeting.cs
@@ -8,8 +8,8 @@ namespace CourseMeetingEntitiesLib.dbo
         public int MID{ get; set; }
         public int CID { get; set; }
         public Course Course { get; set; }
-        public DateTime DateOfTheMeeting { get; set; }
-        public int MaxParticpants { get; set; }
+        public DateTime DateOfTheMeeting { get; set; }  
+        public int MaxParticipants { get; set; }
         public IList<CourseMeetingParticipant> CourseMeetingParticipants { get; set; }
     }
 }
