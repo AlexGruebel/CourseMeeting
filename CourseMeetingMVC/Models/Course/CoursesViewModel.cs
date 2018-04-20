218f89d7e8a4821465423a2f4a77d91ebd03f040
AlexGruebel <alexander-gruembel@online.de>
2018-04-20 07:36:45 +0200

    add CoursesViewModel

---
+++ CourseMeetingMVC/Models/Course/CoursesViewModel.cs
@@ -0,0 +1,8 @@
+using CourseMeetingEntitiesLib.dbo;
+namespace CourseMeetingMVC.Models.Course
+{
+    public class CoursesViewModel
+    {
+        public CourseMeetingEntitiesLib.dbo.Course[] Courses {get;set;}
+    }
+}
