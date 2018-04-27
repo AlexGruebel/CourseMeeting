08468a35ace29b566b54396b2622bdf347d110b2
AlexGruebel <alexander-gruembel@online.de>
2018-04-27 14:11:40 +0200

    start identity at 1

--- CourseMeetingDB/Tables/dbo.CourseMeetings.sql
+++ CourseMeetingDB/Tables/dbo.CourseMeetings.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[CourseMeetings]
 (
-	[MID] INT IDENTITY(0,1) PRIMARY KEY
+	[MID] INT IDENTITY(1,1) PRIMARY KEY
    ,[CID] INT FOREIGN KEY REFERENCES Courses (CID)
    ,[DateOfTheMeeting] datetime2 not null
    ,[MaxParticipants] int not null
