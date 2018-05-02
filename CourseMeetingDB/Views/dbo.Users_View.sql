25082c12dd9f777920d6720fba45961119caced4
AlexGruebel <alexander-gruembel@online.de>
2018-05-02 07:29:05 +0200

    add dbo.Users View

--- CourseMeetingDB/Views/dbo.Users_View.sql
+++ CourseMeetingDB/Views/dbo.Users_View.sql
@@ -1,6 +1,8 @@
-create VIEW [dbo].[Users_View] AS
-SELECT [UID]
+Create VIEW [dbo].[Users_View] AS
+SELECT u.[UID]
       ,[UserName]
+      ,r.[RoleID]
       ,[Email]
       ,[PhoneNumber]
-FROM [CourseMeetingDB].[sec].[Users]
+FROM [CourseMeetingDB].[sec].[Users] u
+join [CourseMeetingDB].[sec].[IdentityUserRole] r on u.UID = r.UserID
