c21b251e5bd8ce2ce37f4d9c74ba5f947e4ab553
AlexGruebel <alexander-gruembel@online.de>
2018-10-04 17:53:56 +0200

    add new asp-actions

--- CourseMeetingMVC/Controllers/CourseController.cs
+++ CourseMeetingMVC/Controllers/CourseController.cs
@@ -16,9 +16,12 @@ namespace CourseMeetingMVC.Controllers
         private readonly CourseMeetingDb _db;
         private readonly UserManager<User> _UserManger;
 
+        private string _UserID;
+
         public CourseController(CourseMeetingDb CourseMeetingDb, UserManager<User> userManager){
             this._db = CourseMeetingDb;
             this._UserManger = userManager;
+            //this._UserID = userManager.GetUserId(User);
         }
 
         [HttpGet]
@@ -48,7 +51,7 @@ namespace CourseMeetingMVC.Controllers
             return View(model);
         }
 
-        [HttpPost]
+        [HttpGet]
         [Authorize(Roles = "Student")]
         public async Task<IActionResult> SignInToCourseMeeting(int id){
             var participant = new CourseMeetingParticipant{
@@ -58,6 +61,63 @@ namespace CourseMeetingMVC.Controllers
             _db.CourseMeetingParticipants.Add(participant);
             await _db.SaveChangesAsync();
             return Redirect("/Course/Courses");
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles = "Student")]
+        public async Task<IActionResult> MyMeetings()
+        {
+            this._UserID = await this._UserManger.GetUserIdAsync(await this._UserManger.GetUserAsync(User));
+            var model = new MyMeetingsViewModel{
+                CourseMeetings = await this._db.CourseMeetings.Include(c => c.Course).Join( (_db.CourseMeetingParticipants.Where(p => p.SUID == this._UserID))
+                                           ,c => c.MID
+                                           ,p => p.MID
+                                           ,(c, p) => c)
+                                           .Include(c => c.Teacher)
+                                           .ToArrayAsync(),
+            };                                           
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles = "Student")]
+        public async Task<IActionResult> Unsubscribe(int id)
+        {
+            this._UserID = await this._UserManger.GetUserIdAsync(await this._UserManger.GetUserAsync(User));            
+            var mp = await _db.CourseMeetingParticipants.Where(m => m.MID == id && m.SUID == this._UserID)
+                    .FirstOrDefaultAsync();
+            this._db.CourseMeetingParticipants
+                .Remove(mp);
+
+            await _db.SaveChangesAsync();
+            return Ok();
+        }
+
+
+        [HttpGet]
+        [Authorize(Roles="Teacher")]
+        public async Task<IActionResult> MyCourses(){
+            this._UserID = await this._UserManger.GetUserIdAsync(await this._UserManger.GetUserAsync(User));            
+
+            var model = new MyCoursesViewModel{
+                Courses = await this._db.Courses
+                                    .Where(c => c.PTUID == this._UserID)
+                                    .ToArrayAsync(),
+                CourseMeetings = await this._db.CourseMeetings
+                                            .Where(c => c.TUID == this._UserID)
+                                            .ToArrayAsync(),
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [Authorize(Roles="Teacher")]
+        public async Task<IActionResult> CreateCourse(string json)
+        {
+            System.Console.WriteLine(json);
+            await this._db.SaveChangesAsync();
+            return Ok();
         }
     }
 }
